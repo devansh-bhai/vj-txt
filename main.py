3fab4a173a2c1b26e179147752475f37b59ae293
devansh-bhai <163751936+devansh-bhai@users.noreply.github.com>
2024-04-23 20:10:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -357,12 +357,12 @@ async def account_login(bot: Client, m: Message):
              url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
 
             
-            elif 'api.zoom.us' in url:
-                        parsed_url = urlparse(url)
-                        query_params = parse_qs(parsed_url.query)
+            # elif 'api.zoom.us' in url:
+            #             parsed_url = urlparse(url)
+            #             query_params = parse_qs(parsed_url.query)
     
-                        if 'originRequestUrl' in query_params:
-                            url = query_params['originRequestUrl'][0]
+            #             if 'originRequestUrl' in query_params:
+            #                 url = query_params['originRequestUrl'][0]
                             # print(url)
                             # return url
                                          
